9d5b6fcd11f0d4491a801ea1c9b8eccce0d5fe87
Jordan SINTES <jordan.sintes@gmail.com>
2019-04-26 11:45:46 +0200

    Record is now working (to improve)

--- src/pedal-board/src/wc-multitrack/js.js
+++ src/pedal-board/src/wc-multitrack/js.js
@@ -12,11 +12,13 @@
       super();
 
       //Global Context of Pedalboard
-      this.gc=GlobalContext;
-      
+      this.gc = GlobalContext;
+
       //Graphic Element
+      const parent = this;
       this.recordButton;
       this.stopButton;
+      this.dlButton;
       this.canvas;
 
       //Audio element
@@ -37,8 +39,9 @@
       this.link;
 
       this.params = {
-      
+
       }
+
     }
 
     get is() { return this.nodeName.toLowerCase(); }
@@ -72,186 +75,182 @@
       const instance = template.content.cloneNode(true);
       shadowRoot.appendChild(instance);
 
-    }
-
-    // ----- METHODS: CUSTOM -----
-    
-    getElement(){
-      this.recordButton = document.querySelector('#record');
-      this.stopButton = document.querySelector('#stop');
-      this.canvas = document.querySelector('#canvas');
-
+      //Extract attribute
+      let parent = this;
+      this.recordButton = this.shadowRoot.querySelector('#record');
+      this.stopButton = this.shadowRoot.querySelector('#stop');
+      this.dlButton = this.shadowRoot.querySelector('#download')
+      this.canvas = this.shadowRoot.querySelector('canvas');
       navigator.mediaDevices.getUserMedia({
         audio: true
       })
-      .then(function (stream) {
-    
-        this.recordButton.disabled = false;
-        this.recordButton.addEventListener('click', startRecording);
-        this.stopButton.addEventListener('click', stopRecording);
-        this.recorder = new MediaRecorder(stream);
-        this.recorder.addEventListener('dataavailable', onRecordingReady);
-        this.recorder.addEventListener('start', onRecordingStarted);
-    
-    
-        console.log('recorder is ready');
-    });
-  }
+        .then(function (stream) {
+          parent.recordButton.disabled = false;
+          parent.recordButton.addEventListener('click', parent.startRecording.bind(parent));
+          parent.stopButton.addEventListener('click', parent.stopRecording.bind(parent));
+          parent.dlButton.addEventListener('click', parent.download.bind(parent));
+          parent.recorder = new MediaRecorder(stream);
+          parent.recorder.addEventListener('dataavailable', parent.onRecordingReady.bind(parent));
 
-  startRecording(){
-    console.log('start recording');
-    this.recordButton.disabled = true;
-    this.stopButton.disabled = false;
-
-    this.stopSample();
-
-    this.recorder.start();
-  }
-
-  stopRecording(){
-    console.log('stop recording');
-    this.recordButton.disabled= false;
-    this.stopButton.disabled= true;
-
-    this.recorder.stop()
-  }
-  stopSample(){
-    console.log('record something');
-    this.startRecording();
-  }
-
-  onRecordingReady(e){
-    let parent = this;
-    console.log("onRecordingReady");
-    this.blob= e.data;
-    this.recordedBlobs.push(e.data);
-
-    this.fileReader = new FileReader();
-    this.fileReader.onprogress= function(){
-      console.log("recording...")
+          console.log('recorder is ready');
+        });
     }
-    this.fileReader.onload= function(){
-      parent.arrayBuffer = this.result;
-      parent.ac.decodeAudioData(parent.arrayBuffer, function(decoded){
-        console.log('finished!', decoded.length);
-        parent.useSample(decoded);
-      }, function(fail){
-        console.error('fail!', fail);
-      });
-    }
-    this.fileReader.readAsArrayBuffer(this.blob);
-  }
+    // ----- METHODS: CUSTOM -----
 
-  useSample(sample){
-    if(this.bufferSourceNode){
-      this.bufferSourceNode.stop();
-      this.bufferSourceNode.disconnect()
+    startRecording() {
+      console.log('start recording');
+      this.recordButton.disabled = true;
+      this.stopButton.disabled = false;
+
+      this.stopSample();
+
+      this.recorder.start();
     }
 
-    this.bufferSourceNode = this.ac.createBufferSource();
-    this.bufferSourceNode.loop = true;
-    this.bufferSourceNode.connect(this.limiter);
-    this.bufferSourceNode.start();
+    stopRecording() {
+      console.log('stop recording');
+      this.recordButton.disabled = false;
+      this.stopButton.disabled = true;
 
-    this.data = [];
-    this.channelData =  sample.getChannelData(0);
-
-    for(let i = 0; i< this.channelData.length; i++){
-      this.data.push(this.channelData[i]);
+      this.recorder.stop()
+    }
+    stopSample() {
+      console.log('record something');
+      this.startRecording();
     }
 
-    this.RenderWave(this.canvas, this.data);
-  }
+    onRecordingReady(e) {
+      let parent = this;
+      console.log("onRecordingReady");
+      this.blob = e.data;
+      this.recordedBlobs.push(e.data);
 
-  download() {
-    let parent= this;
-    this.blob = new Blob(this.recordedBlobs, {type: 'audio/wav'});
-    this.url = window.URL.createObjectURL(blob);
-    this.link = document.createElement('a');
-    this.link.style.display='none';
-    this.link.href = this.url;
-    this.link.download = 'pedalboard_song.wav';
-    document.body.appendChild(this.link);
-    this.link.click();
-    
-    setTimeout(function() {
-      document.body.removeChild(parent.link);
-      window.URL.revokeObjectURL(parent.url);
-    }, 100);
-  }
-
-  stopSample(){
-    console.log('stop sample');
-    if(this.bufferSourceNode){
-      this.bufferSourceNode.stop();
-      this.bufferSourceNode.disconnect();
-      this.bufferSourceNode = undefined;
+      this.fileReader = new FileReader();
+      this.fileReader.onprogress = function () {
+        console.log("recording...")
+      }
+      this.fileReader.onload = function () {
+        parent.arrayBuffer = this.result;
+        parent.ac.decodeAudioData(parent.arrayBuffer, function (decoded) {
+          console.log('finished!', decoded.length);
+          parent.useSample(decoded);
+        }, function (fail) {
+          console.error('fail!', fail);
+        });
+      }
+      this.fileReader.readAsArrayBuffer(this.blob);
     }
-  }
 
-  RenderWave(canvas, data){
-    let context = this.canvas.getContext('2d');
-    let canvasWidth = this.canvas.width;
-    let canvasHeigth = this.canvas.width;
-    let canvasHalfHeight = canvasHeigth * 0.5;
-
-    this.bufferLength = this.data.length;
-
-    context.fillStyle = 'rgb(255, 0, 0)';
-    context.fillRect(0, 0, canvasWidth, canvasHeigth);
-
-    context.lineWidth = 1;
-    context.strokeStyle = 'rgb(0, 0, 0)';
-
-    context.beginPath();
-
-    let sliceWidth = vanvasWidth * 1.0 / this.bufferLength;
-    let x = 0 - sliceWidth;
-
-    for(let i = 0; i < this.bufferLength; i++){
-      let v = 1 - this.data[i];
-      let y = v * canvasHalfHeight;
-
-      if (i === 0){
-        context.moveTo(x, y);
-      } else {
-        context.lineTo(x, y);
+    useSample(sample) {
+      if (this.bufferSourceNode) {
+        this.bufferSourceNode.stop();
+        this.bufferSourceNode.disconnect()
       }
 
-      x += sliceWidth;
+      this.bufferSourceNode = this.ac.createBufferSource();
+      this.bufferSourceNode.loop = true;
+      this.bufferSourceNode.connect(this.limiter);
+      this.bufferSourceNode.start();
+
+      this.data = [];
+      this.channelData = sample.getChannelData(0);
+
+      for (let i = 0; i < this.channelData.length; i++) {
+        this.data.push(this.channelData[i]);
+      }
+
+      this.RenderWave(this.canvas, this.data);
     }
 
-    context.lineTo(canvasWidth, canvasHalfHeight);
+    download() {
+      let parent = this;
+      this.blob = new Blob(this.recordedBlobs, { type: 'audio/wav' });
+      this.url = window.URL.createObjectURL(this.blob);
+      this.link = document.createElement('a');
+      this.link.style.display = 'none';
+      this.link.href = this.url;
+      this.link.download = 'pedalboard_song.wav';
+      document.body.appendChild(this.link);
+      this.link.click();
 
-    context.stroke();
-  }
+      setTimeout(function () {
+        document.body.removeChild(parent.link);
+        window.URL.revokeObjectURL(parent.url);
+      }, 100);
+    }
 
-  MaximiseSampleInPlace(sample) {
-    console.log('maximise sample in place');
-    let length = sample.length;
-    let numChannels = sample.numberOfChannels;
-    
-    let maxValue = 0;
-  
-    for(let i = 0; i < numChannels; i++) {
-      this.data = sample.getChannelData(i);
-  
-      for(let j = 0; j < length; j++) {
-        let value = Math.abs(this.data[j]);
-        maxValue = Math.max(value, maxValue);
+    stopSample() {
+      console.log('stop sample');
+      if (this.bufferSourceNode) {
+        this.bufferSourceNode.stop();
+        this.bufferSourceNode.disconnect();
+        this.bufferSourceNode = undefined;
       }
     }
-  
-    let amp = 1.0 / maxValue;
-  
-    for(let i = 0; i < numChannels; i++) {
-    //let inData = sample.getChannelData(i);
-      for(let j = 0; j < length; j++) {
-        let value = this.data[j];
-        this.data[j] = value * amp;
+
+    RenderWave(canvas, data) {
+      let context = this.canvas.getContext('2d');
+      let canvasWidth = this.canvas.width;
+      let canvasHeigth = this.canvas.height;
+      let canvasHalfHeight = canvasHeigth * 0.5;
+
+      this.bufferLength = data.length;
+
+      context.fillStyle = 'rgb(255, 255, 255)';
+      context.fillRect(0, 0, canvasWidth, canvasHeigth);
+
+      context.lineWidth = 1;
+      context.strokeStyle = 'rgb(0, 0, 0)';
+
+      context.beginPath();
+
+      let sliceWidth = canvasWidth * 1.0 / this.bufferLength;
+      let x = 0 - sliceWidth;
+
+      for (let i = 0; i < this.bufferLength; i++) {
+        let v = 1 - this.data[i];
+        let y = v * canvasHalfHeight;
+
+        if (i === 0) {
+          context.moveTo(x, y);
+        } else {
+          context.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      context.lineTo(canvasWidth, canvasHalfHeight);
+
+      context.stroke();
+    }
+
+    MaximiseSampleInPlace(sample) {
+      console.log('maximise sample in place');
+      let length = sample.length;
+      let numChannels = sample.numberOfChannels;
+
+      let maxValue = 0;
+
+      for (let i = 0; i < numChannels; i++) {
+        this.data = sample.getChannelData(i);
+
+        for (let j = 0; j < length; j++) {
+          let value = Math.abs(this.data[j]);
+          maxValue = Math.max(value, maxValue);
+        }
+      }
+
+      let amp = 1.0 / maxValue;
+
+      for (let i = 0; i < numChannels; i++) {
+        //let inData = sample.getChannelData(i);
+        for (let j = 0; j < length; j++) {
+          let value = this.data[j];
+          this.data[j] = value * amp;
+        }
       }
     }
-  }
 
   });
 })();
